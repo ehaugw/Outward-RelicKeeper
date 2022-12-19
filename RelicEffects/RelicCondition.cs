52d92b0419bf49d636a024b1215c131a97c148f9
Eivind <ehaugw@gmail.com>
2022-12-19 17:25:26 +0100

    update reliccondition base to update cooldown, stamina cost, mana cost and health cost

--- RelicEffects/RelicCondition.cs
+++ RelicEffects/RelicCondition.cs
@@ -1,4 +1,4 @@
-﻿using EffectSourceConditions;
+using EffectSourceConditions;
 using InstanceIDs;
 using SideLoader;
 using System;
@@ -13,20 +13,19 @@ namespace RelicKeeper
 {
     public static class RelicCondition
     {
-        public static Transform Apply(Skill skill, int requiredItem, float manaCost=0, float durabilityCost=0)
+        public static Transform Apply(Skill skill, int requiredItem, float manaCost=0, float cooldown=2, float staminaCost=0, float healthCost=0, float durabilityCost=0)
         {
+            
             var effectsContainer = TinyGameObjectManager.MakeFreshObject(EffectSourceConditions.EffectSourceConditions.EFFECTS_CONTAINER, true, true, skill.transform).transform;
             var requirementTransform = TinyGameObjectManager.GetOrMake(effectsContainer.transform, EffectSourceConditions.EffectSourceConditions.SOURCE_CONDITION_CONTAINER, true, true);
             var skillReq = requirementTransform.gameObject.AddComponent<SourceConditionEquipment>();
             skillReq.RequiredItemID = requiredItem;
 
-            if (manaCost > 0)
-            {
-                var manaCostTrans = requirementTransform.gameObject.AddComponent<SourceConditionMana>();
-                manaCostTrans.ManaCost = manaCost;
-
-                effectsContainer.gameObject.AddComponent<UseMana>().UsedMana = manaCost;
-            }
+            var dynamicSkillStat = requirementTransform.gameObject.AddComponent<DynamicSkillStat>();
+            dynamicSkillStat.ManaCost = manaCost;
+            dynamicSkillStat.HealthCost = healthCost;
+            dynamicSkillStat.StaminaCost= staminaCost;
+            dynamicSkillStat.Cooldown = cooldown;
 
             return effectsContainer;
         }
