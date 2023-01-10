5b272dc3e32de730879d09eb808582b62f093828
Eivind <ehaugw@gmail.com>
2023-01-10 21:27:43 +0100

    create helper to get list of active relics

---
+++ RelicBehavior.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using InstanceIDs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TinyHelper;
+
+namespace RelicKeeper
+{
+    public static class RelicBehavior
+    {
+        public static List<Item> GetEquippedRelics(Character character)
+        {
+            var result = character.Inventory.GetOwnedItems(TinyTagManager.GetOrMakeTag(IDs.RelicTag)).Where(x => x.DisplayedOnBag).ToList();
+            if (character.LeftHandEquipment?.HasTag(TinyTagManager.GetOrMakeTag(IDs.RelicTag)) ?? false)
+            {
+                result.Add(character.LeftHandEquipment);
+            }
+            return result;
+        }
+    }
+}
