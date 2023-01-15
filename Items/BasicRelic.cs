1394d6ccd0b04942da1ab0e08129ccd0fb63e0b1
Eivind <ehaugw@gmail.com>
2023-01-15 13:28:06 +0100

    update basic relic position

--- Items/BasicRelic.cs
+++ Items/BasicRelic.cs
@@ -9,6 +9,7 @@ namespace RelicKeeper
     using SideLoader;
     using EffectSourceConditions;
     using System;
+    using UnityEngine;
 
     public class BasicRelic
     {
@@ -27,7 +28,7 @@ namespace RelicKeeper
                 Description = "",
                 StatsHolder = new SL_ItemStats()
                 {
-                    MaxDurability = 100,
+                    MaxDurability = 20,
                 },
                 BehaviorOnNoDurability = Item.BehaviorOnNoDurabilityType.Destroy,
                 RepairedInRest = false,
@@ -47,10 +48,13 @@ namespace RelicKeeper
                     Prefab_Name = "basic_relic_Prefab",
                     Prefab_AssetBundle = "basic_relic",
                     Prefab_SLPack = RelicKeeper.ModFolderName,
-                }
+                    Rotation = new Vector3(-90, 0, 0),
+                    Position = new Vector3(0, -0.093f, 0),
+                },
             };
             myitem.ApplyTemplate();
             var item = ResourcesPrefabManager.Instance.GetItemPrefab(myitem.New_ItemID) as Equipment;
+            item.BagCategorySlot = Item.BagCategorySlotType.Lantern;
             item.IKType = Equipment.IKMode.None;
 
             return item as Item;
