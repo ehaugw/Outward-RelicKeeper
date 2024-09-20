04f0276d37e3338ba857f201163ce037c409d2f3
Eivind <ehaugw@gmail.com>
2024-09-20 14:57:11 +0200

    disable harmalan dialogue

--- SkillTreesAndTrainers/HarmalanNPC.cs
+++ SkillTreesAndTrainers/HarmalanNPC.cs
@@ -36,45 +36,45 @@ namespace RelicKeeper
             base(identifierName, rpcListenerID, defaultEquipment: defaultEquipment, moddedEquipment: moddedEquipment, scale: scale, faction: faction, visualData: visualData)
         { }
 
-        override public object SetupClientSide(int rpcListenerID, string instanceUID, int sceneViewID, int recursionCount, string rpcMeta)
-        {
-            Character player = GameObject.FindObjectsOfType<Character>().Where(x => x.IsLocalPlayer).First();
-            Character instanceCharacter = base.SetupClientSide(rpcListenerID, instanceUID, sceneViewID, recursionCount, rpcMeta) as Character;
-            if (instanceCharacter == null) return null;
+        //override public object SetupClientSide(int rpcListenerID, string instanceUID, int sceneViewID, int recursionCount, string rpcMeta)
+        //{
+        //    //Character player = GameObject.FindObjectsOfType<Character>().Where(x => x.IsLocalPlayer).First();
+        //    //Character instanceCharacter = base.SetupClientSide(rpcListenerID, instanceUID, sceneViewID, recursionCount, rpcMeta) as Character;
+        //    //if (instanceCharacter == null) return null;
 
-            GameObject instanceGameObject = instanceCharacter.gameObject;
-            var trainerTemplate = TinyDialogueManager.AssignTrainerTemplate(instanceGameObject.transform);
-            var actor = TinyDialogueManager.SetDialogueActorName(trainerTemplate, IdentifierName);
-            var trainerComp = TinyDialogueManager.SetTrainerSkillTree(trainerTemplate, RelicKeeperSkillTree.RelicKeeperSkillSchool.UID);
-            var graph = TinyDialogueManager.GetDialogueGraph(trainerTemplate);
-            TinyDialogueManager.SetActorReference(graph, actor);
+        //    //GameObject instanceGameObject = instanceCharacter.gameObject;
+        //    //var trainerTemplate = TinyDialogueManager.AssignTrainerTemplate(instanceGameObject.transform);
+        //    //var actor = TinyDialogueManager.SetDialogueActorName(trainerTemplate, IdentifierName);
+        //    //var trainerComp = TinyDialogueManager.SetTrainerSkillTree(trainerTemplate, RelicKeeperSkillTree.RelicKeeperSkillSchool.UID);
+        //    //var graph = TinyDialogueManager.GetDialogueGraph(trainerTemplate);
+        //    //TinyDialogueManager.SetActorReference(graph, actor);
 
-            var rootStatement = TinyDialogueManager.MakeStatementNode(graph, IdentifierName, "Have you came to honor the memories of our ancestors?");
-            var characterIntroduction = TinyDialogueManager.MakeStatementNode(graph, IdentifierName, "That is classified. All I can tell you is that I am Ignacio.");
+        //    //var rootStatement = TinyDialogueManager.MakeStatementNode(graph, IdentifierName, "Have you came to honor the memories of our ancestors?");
+        //    //var characterIntroduction = TinyDialogueManager.MakeStatementNode(graph, IdentifierName, "That is classified. All I can tell you is that I am Ignacio.");
 
-            var openTrainer = TinyDialogueManager.MakeTrainDialogueAction(graph, trainerComp);
+        //    //var openTrainer = TinyDialogueManager.MakeTrainDialogueAction(graph, trainerComp);
 
-            var introMultipleChoice = TinyDialogueManager.MakeMultipleChoiceNode(graph, new string[] {
-                "You mean the people responsible for my blood dept? Well, no... Who are you anyways?",
-                "I am here to receive training."
-            });
+        //    //var introMultipleChoice = TinyDialogueManager.MakeMultipleChoiceNode(graph, new string[] {
+        //    //    "You mean the people responsible for my blood dept? Well, no... Who are you anyways?",
+        //    //    "I am here to receive training."
+        //    //});
 
-            graph.allNodes.Clear();
-            graph.allNodes.Add(rootStatement);
-            graph.allNodes.Add(introMultipleChoice);
-            graph.allNodes.Add(characterIntroduction);
-            graph.allNodes.Add(openTrainer);
+        //    //graph.allNodes.Clear();
+        //    //graph.allNodes.Add(rootStatement);
+        //    //graph.allNodes.Add(introMultipleChoice);
+        //    //graph.allNodes.Add(characterIntroduction);
+        //    //graph.allNodes.Add(openTrainer);
 
-            graph.primeNode = rootStatement;
-            graph.ConnectNodes(rootStatement, introMultipleChoice);
-            graph.ConnectNodes(introMultipleChoice, characterIntroduction, 0);
-            graph.ConnectNodes(introMultipleChoice, openTrainer, 1);
-            graph.ConnectNodes(characterIntroduction, rootStatement);
+        //    //graph.primeNode = rootStatement;
+        //    //graph.ConnectNodes(rootStatement, introMultipleChoice);
+        //    //graph.ConnectNodes(introMultipleChoice, characterIntroduction, 0);
+        //    //graph.ConnectNodes(introMultipleChoice, openTrainer, 1);
+        //    //graph.ConnectNodes(characterIntroduction, rootStatement);
 
-            var obj = instanceGameObject.transform.parent.gameObject;
-            obj.SetActive(true);
+        //    //var obj = instanceGameObject.transform.parent.gameObject;
+        //    //obj.SetActive(true);
 
-            return instanceCharacter;
-        }
+        //    return instanceCharacter;
+        //}
     }
 }
