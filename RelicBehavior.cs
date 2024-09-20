f722a8c359588a12c48a3c604626780db279c4bf
Eivind <ehaugw@gmail.com>
2024-09-20 12:36:00 +0200

    move HasRelicEquippedOrOnBackpack to RelicBehaviors

--- RelicBehavior.cs
+++ RelicBehavior.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿using CharacterExtensions;
+using HarmonyLib;
 using InstanceIDs;
 using System;
 using System.Collections.Generic;
@@ -19,6 +20,26 @@ namespace RelicKeeper
                 result.Add(character.LeftHandEquipment);
             }
             return result;
+        }
+
+        public static bool HasRelicEquippedOrOnBackpack(Character character, int RequiredItemID = 0, int RequiredEnchantID = 0)
+        {
+            if (character?.Inventory?.Equipment is CharacterEquipment characterEquipment)
+            {
+                var potentialEquipment = characterEquipment.EquipmentSlots.Where(s => s != null && s.EquippedItem != null).Select(s => s.EquippedItem).ToList();
+
+                foreach (var slot in character.EquippedOnBag().Union(potentialEquipment))
+                {
+                    var matchingItem = RequiredItemID == 0 || slot.ItemID == RequiredItemID;
+                    var matchingEnchant = RequiredEnchantID == 0 || (slot is Equipment equipmentslot && equipmentslot.ActiveEnchantmentIDs.Contains(RequiredEnchantID));
+
+                    if (matchingItem && matchingEnchant)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
 }
--- RelicPassives.cs
+++ RelicPassives.cs
@@ -17,26 +17,6 @@ namespace RelicKeeper
         {
             return ((character?.Inventory?.Equipment?.ItemEquippedCount(IDs.woodooCharmID) ?? 0) > 0) && TinyHelper.SkillRequirements.SafeHasSkillKnowledge(character, IDs.manaFlowID);
         }
-
-        public static bool HasRelicEquippedOrOnBackpack(Character character, int RequiredItemID = 0, int RequiredEnchantID = 0)
-        {
-            if (character?.Inventory?.Equipment is CharacterEquipment characterEquipment)
-            {
-                var potentialEquipment = characterEquipment.EquipmentSlots.Where(s => s != null && s.EquippedItem != null).Select(s => s.EquippedItem).ToList();
-
-                foreach (var slot in character.EquippedOnBag().Union(potentialEquipment))
-                {
-                    var matchingItem = RequiredItemID == 0 || slot.ItemID == RequiredItemID;
-                    var matchingEnchant = RequiredEnchantID == 0 || (slot is Equipment equipmentslot && equipmentslot.ActiveEnchantmentIDs.Contains(RequiredEnchantID));
-
-                    if (matchingItem && matchingEnchant)
-                    {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
     }
 
     [HarmonyPatch(typeof(CharacterEquipment), "GetTotalManaUseModifier")]
