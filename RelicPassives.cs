6293a87e0c42571afbf73274a5ebccfb4a597941
Eivind <ehaugw@gmail.com>
2024-09-20 12:31:48 +0200

    implement HasRelicEquippedOrOnBackpack

--- RelicPassives.cs
+++ RelicPassives.cs
@@ -5,14 +5,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TinyHelper;
 
 namespace RelicKeeper
 {
+    using CharacterExtensions;
+
     public class RelicPassives
     {
         public static bool CanCastTormentWithDrawnWeapon(Character character)
         {
             return ((character?.Inventory?.Equipment?.ItemEquippedCount(IDs.woodooCharmID) ?? 0) > 0) && TinyHelper.SkillRequirements.SafeHasSkillKnowledge(character, IDs.manaFlowID);
+        }
+
+        public static bool HasRelicEquippedOrOnBackpack(Character character, int RequiredItemID = 0, int RequiredEnchantID = 0)
+        {
+            if (character?.Inventory?.Equipment is CharacterEquipment characterEquipment)
+            {
+                var potentialEquipment = characterEquipment.EquipmentSlots.Where(s => s != null && s.EquippedItem != null).Select(s => s.EquippedItem).ToList();
+
+                foreach (var slot in character.EquippedOnBag().Union(potentialEquipment))
+                {
+                    var matchingItem = RequiredItemID == 0 || slot.ItemID == RequiredItemID;
+                    var matchingEnchant = RequiredEnchantID == 0 || (slot is Equipment equipmentslot && equipmentslot.ActiveEnchantmentIDs.Contains(RequiredEnchantID));
+
+                    if (matchingItem && matchingEnchant)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
 
