c86d9da7a6bd54612a5e2a1ffabb2054da3b3fe8
Eivind <ehaugw@gmail.com>
2024-09-20 12:57:20 +0200

    implement SourceConditionRelic

---
+++ SourceConditionalEffects/SourceConditionRelic.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using EffectSourceConditions;
+
+namespace RelicKeeper
+{
+
+    public class SourceConditionRelic : SourceConditionEquipment
+    {
+        /// <summary>
+        /// The level of the relic that the condition is for. 0 means anyone can use it, 1 means you need relic lore, and 2 means you need mythic lore
+        /// </summary>
+        public int relicLevel;
+
+        /// <summary>
+        /// Returns true if RequiredSkillID <= 0 or if the character knows the Skill with ItemID = RequiredSkillID
+        /// </summary>
+        /// <param name="character"></param>
+        /// <returns></returns>
+        public override bool CharacterHasRequirement(Character character)
+        {
+            int characterRelicLevel = 0;
+            if (TinyHelper.SkillRequirements.SafeHasSkillKnowledge(character, InstanceIDs.IDs.relicLoreID))
+                characterRelicLevel = 1;
+            if (TinyHelper.SkillRequirements.SafeHasSkillKnowledge(character, InstanceIDs.IDs.mythicLoreID))
+                characterRelicLevel = 2;
+
+            return (characterRelicLevel >= relicLevel) ^ Inverted && base.CharacterHasRequirement(character);
+        }
+    }
+}
