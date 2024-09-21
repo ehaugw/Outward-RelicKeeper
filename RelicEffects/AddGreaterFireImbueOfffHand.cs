5040f7817325377d8c99c774ba72ca0f190160c3
Eivind <ehaugw@gmail.com>
2024-09-21 16:40:00 +0200

    make add fire imbue effect stationary to use the correct animation

--- RelicEffects/AddGreaterFireImbueOfffHand.cs
+++ RelicEffects/AddGreaterFireImbueOfffHand.cs
@@ -8,7 +8,7 @@ namespace RelicKeeper
     {
         public static void Apply(Skill skill, int requiredItem)
         {
-            var relicCondition = RelicCondition.Apply(skill, requiredItem, manaCost: 14, durabilityCost: 50, cooldown: 200, castType: Character.SpellCastType.Bubble, relicLevel: 1);
+            var relicCondition = RelicCondition.Apply(skill, requiredItem, manaCost: 14, durabilityCost: 50, cooldown: 200, castType: Character.SpellCastType.Bubble, relicLevel: 1, mobileCastMovementMult: 0, castModifier: Character.SpellCastModifier.Immobilized);
             
             var addImbue = relicCondition.EffectsContainer.gameObject.AddComponent<ImbueWeapon>();
             addImbue.ImbuedEffect = ResourcesPrefabManager.Instance.GetEffectPreset(IDs.fireVarnishImbueID) as ImbueEffectPreset;
